5c08d5a2a914b354bd462d5ee4030b830fd09838
Chadd <chadd_artaud@cox.net>
2022-04-26 12:34:19 -0700

    added google map api

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function initMap() {
+    var options = {
+        zoom: 8,
+        center: {lat:32.715736, lng: -117.161087}
+    }
+    var map = new google.maps.Map(document.querySelector('#map'), options)
+}
+
