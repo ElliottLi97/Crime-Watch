d9c3cad4696e467e911c4d371037970faf52d838
Elliott <elliottli.job@gmail.com>
2022-04-26 17:29:25 -0700

    added api call for Crime Data

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,27 @@ function initMap() {
     var map = new google.maps.Map(document.querySelector('#map'), options)
 }
 
+
+// Crime Data API key: gpuXbzy7VI8nN51pmvGzSPPYl1TeGQa16HiOiSn5 We're only limited to 100 calls on this key
+var lat = "33.1434" //xx.yyyy
+var lon = "-117.1661"
+var distance = "5mi" // Xunits unit types: mi yd ft km m 
+var datetime_ini = "2022-01-01 00:00:00" // yyyy-MM-dd'T'HH: mm: ss.SSS'Z or YYYY-MM-DD HH:mm:ss
+var datetime_end = "2022-04-25 00:00:00"
+
+var request = new XMLHttpRequest();
+
+request.open('GET', 'https://api.crimeometer.com/v1/incidents/raw-data?lat='+lat+'&lon='+lon+'&distance='+distance+'&datetime_ini='+datetime_ini+'&datetime_end='+datetime_end+'&page=1');
+
+request.setRequestHeader('Content-Type', 'application/json');
+request.setRequestHeader('x-api-key', 'gpuXbzy7VI8nN51pmvGzSPPYl1TeGQa16HiOiSn5');
+
+request.onreadystatechange = function () {
+  if (this.readyState === 4) {
+    console.log('Status:', this.status);
+    console.log('Headers:', this.getAllResponseHeaders());
+    console.log('Body:', this.responseText);
+  }
+};
+
+//request.send(); 
